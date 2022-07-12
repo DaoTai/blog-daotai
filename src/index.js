dd74f05704453e11d3e0c508d93b7835b7ff1591
DaoTai <88814089+DaoTai@users.noreply.github.com>
2022-07-12 09:44:38 +0700

    OK

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Config middleware
+app.use(express.urlencoded({
+  extended: true
+})) //xử lý dữ liệu được submit khi sử dụng thẻ <form></form>
+app.use(express.json()) //xử lý dữ liệu khi sử dụng JS để submit
+
+// HTTP logger
+// app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resource/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.render('search');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
