4011d540464b73f7be41765af01c34db82ebc421
DaoTai <88814089+DaoTai@users.noreply.github.com>
2022-07-27 11:11:09 +0700

    automatically increasement _id

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,8 +4,14 @@ const {multipleMongooseToObject} = require('../../util/mongoose')
 class MeController {
      // [GET] me/stored/courses
     storedCourses(req, res, next) {
+        let courseQuery = Course.find({})
+        if(req.query.hasOwnProperty('_sort')){
+            courseQuery.sort({
+                [req.query.column]: req.query.type
+            })
+        }
 
-        Promise.all([Course.find({}), Course.countDocumentsDeleted({})])
+        Promise.all([courseQuery, Course.countDocumentsDeleted({})])
             .then(([courses, deletedAmount]) => {
                 res.render('me/stored-courses', {
                     deletedAmount,
@@ -16,7 +22,15 @@ class MeController {
     }
     // [GET] me/trash/courses
     trashCourses(req, res, next){
-        Course.findDeleted({})
+        let courseQuery = Course.findDeleted({})     
+        
+        if(req.query.hasOwnProperty('_sort')){
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type
+            })
+        }
+        
+        courseQuery
             .then(courses => res.render('me/trash-courses', {
                 courses: multipleMongooseToObject(courses)
             }))
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 const mongooseDelete = require('mongoose-delete');
 const slug = require('mongoose-slug-generator');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 // Schema: lược đồ
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
+    _id: {type: Number},
     name: { type: String, default: '', required: true },
     description: { type: String},
     image: { type: String },
     slug: { type: String, slug: "name", unique: true },
     videoId: { type: String },
 },
-{timestamps: true});
+{   timestamps: true,
+    _id: false
+});
 
 // Add plugin
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
     overrideMethods: 'all',
     deletedAt : true,
